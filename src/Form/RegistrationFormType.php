9b8e805ccc0cae8b2237b51633419c5caf8b8b2b
adriraxor <figueres.adrien@gmail.com>
2021-04-18 14:45:12 +0200

    Commit#2 || Minor security update
    ############################
    #      Changes Log         #
    ############################
    -Registration form verification on input increased (Max length, email pattern and more)
    
    ############################
    #       Future update      #
    ############################
    
    -> Increase more esthetic
    -> Add real home page
    -> And more ...

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,9 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Client;
-use EasyCorp\Bundle\EasyAdminBundle\Form\Type\TextEditorType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -19,52 +19,88 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', TextType::class, [
+            ->add('email', EmailType::class, [
                 'label' => false,
                 'attr' => [
                     'placeholder' => 'Adresse Email',
                     'autocomplete' => 'off',
                 ],
+                'constraints' => [
+                    new Length([
+                        'min' => 5,
+                        'minMessage' => 'L\'email doit comporter au minimum {{ limit }} caractères',
+                        'maxMessage' => 'L\'email doit comporter au maximum {{ limit }} caractères',
+                        'max' => 30,
+                    ]),
+                ],
             ])
-
             ->add('country', ChoiceType::class, [
+                'label' => 'Région code : (exemple : FR = France, ES = Espagne)',
                 'choices' => json_decode(
-                    file_get_contents(__DIR__.'/../Data/countries-FR.json'), true
+                    file_get_contents(__DIR__ . '/../Data/countries-FR.json'), true
                 )
             ])
-
             ->add('num_tel', TextType::class, [
                 'label' => false,
                 'attr' => [
                     'placeholder' => 'Numéro de téléphone',
                     'autocomplete' => 'off',
+                    'maxlength' => '10',
+                ],
+                'constraints' => [
+                    new Length([
+                        'min' => 10,
+                        'minMessage' => 'Le numéro de téléphone doit comporter au minimum {{ limit }} caractères',
+                        'maxMessage' => 'Le numéro de téléphone doit comporter au maximum {{ limit }} caractères',
+                        'max' => 10,
+                    ]),
                 ],
             ])
-
             ->add('pseudo', TextType::class, [
                 'label' => false,
                 'attr' => [
                     'placeholder' => 'Pseudonyme',
                     'autocomplete' => 'off',
                 ],
+                'constraints' => [
+                    new Length([
+                        'min' => 5,
+                        'minMessage' => 'Le pseudo doit comporter au minimum {{ limit }} caractères',
+                        'maxMessage' => 'Le pseudo doit comporter au maximum {{ limit }} caractères',
+                        'max' => 20,
+                    ]),
+                ],
             ])
-
             ->add('nom', TextType::class, [
                 'label' => false,
                 'attr' => [
                     'placeholder' => 'Nom de famille',
                     'autocomplete' => 'off',
                 ],
+                'constraints' => [
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Le nom de famille doit comporter au minimum {{ limit }} caractères',
+                        'maxMessage' => 'Le nom de famille doit comporter au maximum {{ limit }} caractères',
+                        'max' => 20,
+                    ]),
+                ],
             ])
-
             ->add('prenom', TextType::class, [
                 'label' => false,
                 'attr' => [
                     'placeholder' => 'Prénom',
                     'autocomplete' => 'off',
                 ],
+                'constraints' => [
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Le prénom doit comporter au minimum {{ limit }} caractères',
+                        'maxMessage' => 'Le prénom doit comporter au maximum {{ limit }} caractères',
+                        'max' => 20,
+                    ]),
+                ],
             ])
-
             ->add('photoProfil', FileType::class, [
                 'required' => true,
                 'attr' => [
@@ -87,7 +123,6 @@ class RegistrationFormType extends AbstractType
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Votre mot de passe doit comporter au minimum {{ limit }} caractères',
-                        // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
